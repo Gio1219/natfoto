6b0b6e461f3f7152f42b1284cafd6aa299db4943
Gio1219 <giovyx1219@gmail.com>
2026-09-04 23:18:24 +0200

    Aggiornato open-next.config.ts con la struttura corretta

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,21 @@
 const config = {
   default: {
     override: {
-      wrapper: "cloudflare",
+      wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
